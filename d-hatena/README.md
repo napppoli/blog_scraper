205dcef4bab1369fc30b5f86e2a01279423db979
napppoli <ryohayashi3571@gmail.com>
2018-02-09 23:45:32 +0100

    directory structure change

---
+++ d-hatena/README.md
@@ -0,0 +1,2 @@
+# blog_scraper
+save your favorite blog posts onto local txt file
---
+++ d-hatena/crawl.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+#coding: utf-8
+from bs4 import BeautifulSoup
+import requests
+def get_body(filename,url):
+    html = requests.get(url)
+    soup = BeautifulSoup(html.content, "lxml").find(id="days")
+    date = soup.find(class_ = "date").string
+    title = soup.find(class_="title").string
+    print(date,title)
+    body = soup.find(class_ = "section").get_text()
+    with open(filename, 'a') as f:
+        f.write('{} {}'.format(date,title))
+        f.write(body+'\n')
