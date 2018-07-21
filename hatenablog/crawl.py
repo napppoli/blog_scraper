970982936805b1b407cec8022903c478d2d599ea
napppoli <ryohayashi3571@gmail.com>
2018-07-21 15:59:33 +0900

    add html mode

--- hatenablog/crawl.py
+++ hatenablog/crawl.py
@@ -2,7 +2,7 @@ from bs4 import BeautifulSoup
 import requests
 import time
 
-def get_body(filename,url):
+def get_body(filename, filetype, url):
     html = requests.get(url)
     time.sleep(1)
     content=html.text.replace('<br />','\n')
@@ -10,7 +10,12 @@ def get_body(filename,url):
     date = soup.find("time").get("datetime")[:10]
     title = soup.find("a", {"class", "entry-title-link bookmark"}).string
     print(date,title)
-    body = soup.find("div",{"class", "entry-content"}).get_text()
-    with open(filename, 'a') as f:
-        f.write('{}\n{}\n'.format(date,title))
-        f.write(body+'\n')
+    if filetype == 'html':
+        dir_name = url.split('.')[0].split('/')[-1]
+        with open('{}/{}.html'.format(dir_name,title), 'w') as f:
+            f.write(html.text)
+    else:
+        body = soup.find("div",{"class", "entry-content"}).get_text()
+        with open(filename, 'a') as f:
+            f.write('{}\n{}\n'.format(date,title))
+            f.write(body+'\n')
--- hatenablog/main.py
+++ hatenablog/main.py
@@ -2,29 +2,38 @@ from bs4 import BeautifulSoup
 import requests
 import argparse
 import time
+import os
 from crawl import get_body
 from next_node import get_url
 
-parser = argparse.ArgumentParser(description='Save blog posts onto local .txt file')
-parser.add_argument('--filename','-f',type=str,required=True,help='filename to save blog posts')
-parser.add_argument('-url',required=True,type=str,help='URL of the blog starting with http://')
-parser.add_argument('-n',type=int,default=100000,help='number of article to save')
+parser = argparse.ArgumentParser(description = 'Save blog posts onto local .txt file')
+parser.add_argument('-url',required = True, type = str,help = 'URL of the blog starting with http://')
+parser.add_argument('-f', '--filename', type = str, default = 'saved_file', help = 'filename to save blog posts')
+parser.add_argument('-n', type = int, default = 100000, help = 'number of article to save')
+parser.add_argument('-t', type = str, default = 'txt', help = 'choose output file type. choices are txt or html')
 args = parser.parse_args()
 
 def main():
     filename = args.filename
     url = args.url
-    suffix= '' if url[-1]=='/' else '/'
-    url+=suffix+'archive'
+    suffix = '' if url[-1]=='/' else '/'
+    url += suffix + 'archive'
     
     html = requests.get(url)
     time.sleep(1)
     soup = BeautifulSoup(html.content, "lxml")
     url = soup.find("a", {"class","entry-title-link"}).get('href')
-    n=args.n
+    n = args.n
+    filetype = args.t
+    if filetype == 'html':
+        dir_name = url.split('.')[0].split('/')[-1]
+        try:
+            os.mkdir(dir_name)
+        except:
+            pass
     for i in range(n):
-        get_body(filename,url)
-        url=get_url(url)
+        get_body(filename, filetype, url)
+        url = get_url(url)
     print('Successfully saved')
 
 if __name__ == '__main__':
